347759aa5b8c3e6d388f4bce8257a3824c4eb438
Admin <benbilldev@gmail.com>
2024-11-25 21:30:40 +0700

    FE - chores: Add dependency

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import LogoutButton from "../components/LogoutButton.jsx"
 export default function HomePage() {
   return (
-    <div><LogoutButton></LogoutButton></div>
+    <div><LogoutButton></LogoutButton>
+    
+    <h1 className='text-3x font-normal'>Dumoe Thanh</h1>
+    </div>
   )
 }
