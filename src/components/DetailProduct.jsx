0f1f8a103d9d00d3c7431bf036c40c9360d5ec75
LeHuuMinh0509 <lehuuminhtc31@gmail.com>
2024-11-26 11:39:30 +0700

    Revert "DetailProduct - jsx - add: Thêm mới file"
    This reverts commit e032509edc0537287efa6fef7d933f9e33c51847.

--- src/components/DetailProduct.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function DetailProduct() {
-    const codeProduct = JSON.parse(localStorage.getItem('CodeProduct'));
-
-  return (
-    <div>Detail Product</div>
-  )
-}
