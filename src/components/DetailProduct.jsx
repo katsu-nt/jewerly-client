e032509edc0537287efa6fef7d933f9e33c51847
LeHuuMinh0509 <lehuuminhtc31@gmail.com>
2024-11-26 11:36:03 +0700

    DetailProduct - jsx - add: Thêm mới file

---
+++ src/components/DetailProduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DetailProduct() {
+    const codeProduct = JSON.parse(localStorage.getItem('CodeProduct'));
+
+  return (
+    <div>Detail Product</div>
+  )
+}
