c9198b575a7b53dd6a4b1d658d618eed9e45a8ac
Admin <benbilldev@gmail.com>
2024-11-27 04:27:18 +0700

    FE - Cart

--- src/APIs/MyCartApi.js
+++ src/APIs/MyCartApi.js
@@ -14,3 +14,45 @@ export const getCart = async (userId) => {
     }
 
 }
+export const addToCart = async (data) => {
+    const response = await fetch(`${API_BASE_URL}/api/my/cart/add`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    if (!response.ok) {
+
+    } else {
+        return response.json()
+    }
+}
+export const removeFromCart = async (data) => {
+    const response = await fetch(`${API_BASE_URL}/api/my/cart/remove`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    if (!response.ok) {
+
+    } else {
+        return response.json()
+    }
+}
+export const removeAll = async (data) => {
+    const response = await fetch(`${API_BASE_URL}/api/my/cart/clear`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    if (!response.ok) {
+
+    } else {
+        return response.json()
+    }
+}
